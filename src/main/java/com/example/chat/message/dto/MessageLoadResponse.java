0df72ddb808146cec3e944bf55d061b8b90d226a
rlwnduu <rlwnduu@gmail.com>
2026-02-10 21:22:47 +0900

    refactor: MessageLoadResponse 롬복 적용
    - @Getter, @Builder, @ToString 등 어노테이션 추가
    
    - 보일러플레이트 코드 제거 및 가독성 향상

--- src/main/java/com/example/chat/message/dto/MessageLoadResponse.java
+++ src/main/java/com/example/chat/message/dto/MessageLoadResponse.java
@@ -1,27 +1,20 @@
 package com.example.chat.message.dto;
 
 import com.example.chat.user.dto.UserInfoResponse;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 import java.util.Map;
 
 @Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageLoadResponse {
 
     private List<MessageView> messages;
     private Map<String, UserInfoResponse> userInfoDtoMap;
     private String nextCursor;
     private boolean hasNext;
-
-    public MessageLoadResponse(List<MessageView> messages,
-                               Map<String, UserInfoResponse> userInfoDtoMap,
-                               String nextCursor,
-                               boolean hasNext
-    ) {
-        this.messages = messages;
-        this.nextCursor = nextCursor;
-        this.hasNext = hasNext;
-        this.userInfoDtoMap = userInfoDtoMap;
-    }
 }
