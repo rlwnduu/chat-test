436e8e9937d1939ce2ff24ee5a2a3ad5e58b874e
rlwnduu <rlwnduu@gmail.com>
2026-02-10 18:17:46 +0900

    refactor: UserInfoResponse DTO 리팩토링
    - 롬복 적용으로 코드 간소화
    
    - 생성자 및 빌더 패턴 적용

--- src/main/java/com/example/chat/user/dto/UserInfoResponse.java
+++ src/main/java/com/example/chat/user/dto/UserInfoResponse.java
@@ -2,9 +2,13 @@ package com.example.chat.user.dto;
 
 import com.example.chat.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoResponse {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING)
@@ -18,23 +22,12 @@ public class UserInfoResponse {
 
     private String profileIconColor;
 
-    public UserInfoResponse() {
-    }
-
     public UserInfoResponse(User user) {
         this.userId = user.getId();
         this.username = user.getUsername();
         this.nickname = user.getNickname();
         this.profileImageUrl = user.getProfileImageUrl();
         this.profileIconColor = user.getProfileIconColor();
-    }
-
-    public UserInfoResponse(Long userId, String username, String nickname, String profileImageUrl, String profileIconColor) {
-        this.userId = userId;
-        this.username = username;
-        this.nickname = nickname;
-        this.profileImageUrl = profileImageUrl;
-        this.profileIconColor = profileIconColor;
     }
 
     public UserInfoResponse(UserInfoProjection projection) {
