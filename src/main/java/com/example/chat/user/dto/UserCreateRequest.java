41fd7f74dc528725ec879fddae9688b552d30feb
rlwnduu <rlwnduu@gmail.com>
2026-02-10 17:35:06 +0900

    refactor: UserCreateRequest DTO 리팩토링
    - 롬복 적용으로 코드 간소화
    
    - ToString에서 비밀번호 필드 제외 처리

--- src/main/java/com/example/chat/user/dto/UserCreateRequest.java
+++ src/main/java/com/example/chat/user/dto/UserCreateRequest.java
@@ -1,22 +1,15 @@
 package com.example.chat.user.dto;
 
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
-@ToString
+@Getter
+@Builder
+@ToString(exclude = "password")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
-
-    @Getter
     private String loginId;
-
-    @Getter
     private String password;
-
-    public UserCreateRequest() {
-    }
-
-    public UserCreateRequest(String loginId, String password) {
-        this.loginId = loginId;
-        this.password = password;
-    }
 }
+
+
