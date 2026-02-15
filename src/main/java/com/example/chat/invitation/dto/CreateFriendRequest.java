39a3a5ff546bba676816f765bb50afab3deb57ee
rlwnduu <rlwnduu@gmail.com>
2026-02-15 22:46:54 +0900

    refactor: CreateFriendRequest DTO 롬복 적용
    - @NoArgsConstructor, @AllArgsConstructor 어노테이션 추가
    
    - 불필요한 수동 생성자 코드 제거

--- src/main/java/com/example/chat/invitation/dto/CreateFriendRequest.java
+++ src/main/java/com/example/chat/invitation/dto/CreateFriendRequest.java
@@ -1,16 +1,12 @@
 package com.example.chat.invitation.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateFriendRequest {
-
     private String username;
-
-    public CreateFriendRequest() {
-    }
-
-    public CreateFriendRequest(String username) {
-        this.username = username;
-    }
 }
